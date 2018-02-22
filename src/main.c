93baa7097785b998dfdd7fe0b5fbfb815f89be78
vicmoh <vimoh1213@gmail.com>
2018-02-22 00:11:17 -0500

    asdf

--- src/main.c
+++ src/main.c
@@ -15,20 +15,33 @@
 
 typedef struct{
     char fileName[256];
+    CPU cpu;
 }Instance;
+
+typedef struct{
+    int process;
+    int thread;
+    int arrivalTime;
+}CPU;
+
+typedef struct{
+    int numberOfProcesses;
+    int threadSwitch;
+    int process
+}INFO;
 
 Instance* initInstance(){
     Instance* newVars = calloc(1, sizeof(Instance));
     return newVars;
 }//end constructor
 
-void readFile(char* fileName){
+void readFile(char* fileName, CPU* cpu){
     FILE* filePointer = fopen(fileName, "r");
-    fprintf(fileName, );
+    fscanf(fileName, "%d %d %d", );
     fclose(filePointer);
 }//end func
 
 int main(int argc, char** argv){
-     
+    
     return 0;
 }//end int
